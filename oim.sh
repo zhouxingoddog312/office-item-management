b4eabbe8bf6a40a16e4a08df9439f37196e07e4f
zhouxin <zhouxin312@qq.com>
2025-09-02 23:19:22 +0800

    add employee

--- oim.sh
+++ oim.sh
@@ -3,26 +3,42 @@
 SCRIPT_DIR=$(dirname "$0")
 LIB_PATH="$SCRIPT_DIR/functions"
 
-WORK_DIR="$home/oim"
+WORK_DIR="$HOME/oim"
 LOG="$WORK_DIR/oim.log"
 METADATA_DIR="$WORK_DIR/data"
+BACKUP_DIR="$WORK_DIR/backup"
 ITEM_FILE="$METADATA_DIR/item"
 EMPLOYEE_FILE="$METADATA_DIR/employee"
 LIST_FILE="$METADATA_DIR/list"
 ############
 
-############创建log文件，输出重定向
-
-############
-
 ############函数库
 source "$LIB_PATH"
+############
+
+###########创建工作目录
+check_dir "$WORK_DIR"
+###########
+
+############创建log文件，输出重定向
+exec 8>>"$LOG"
+############
+
+
+
+############
+help
 version
+install_sed >&8
+install_zenity >&8
+check_dir "$METADATA_DIR" >&8
+check_dir "$BACKUP_DIR" >&8
+add_employee
 ############
 
 
 
 
 ############关闭输出重定向
-
-############
+exec 8>&-
+###########

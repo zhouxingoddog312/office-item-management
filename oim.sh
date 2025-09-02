03be9822586ebf9f1c9d2dd402dee9789340db39
goddog312 <zhouxin312@qq.com>
2025-09-02 12:17:30 +0800

    denpends

--- oim.sh
+++ oim.sh
@@ -5,10 +5,24 @@ LIB_PATH="$SCRIPT_DIR/functions"
 
 WORK_DIR="$home/oim"
 LOG="$WORK_DIR/oim.log"
-METADATA_DIR="$WORK_DIR/"
+METADATA_DIR="$WORK_DIR/data"
+ITEM_FILE="$METADATA_DIR/item"
+EMPLOYEE_FILE="$METADATA_DIR/employee"
+LIST_FILE="$METADATA_DIR/list"
+############
+
+############创建log文件，输出重定向
+
 ############
 
 ############函数库
 source "$LIB_PATH"
 version
+############
+
+
+
+
+############关闭输出重定向
+
 ############
